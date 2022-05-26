a065ed96501763129cf89d6cc68f0e6cdce79979
Maksim Beliavsky <maksimbell503@gmail.com>
2022-05-26 22:23:32 +0300

    add form handler

--- Lab4/RsaDS/DSForm.Designer.cs
+++ Lab4/RsaDS/DSForm.Designer.cs
@@ -39,25 +39,33 @@
             this.tbEnum = new System.Windows.Forms.TextBox();
             this.lblR = new System.Windows.Forms.Label();
             this.lblE = new System.Windows.Forms.Label();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.tbHash = new System.Windows.Forms.TextBox();
+            this.tbSignature = new System.Windows.Forms.TextBox();
+            this.lblText = new System.Windows.Forms.Label();
+            this.lblHash = new System.Windows.Forms.Label();
+            this.lblSignature = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tbPnum
             // 
-            this.tbPnum.Location = new System.Drawing.Point(85, 84);
+            this.tbPnum.Location = new System.Drawing.Point(85, 63);
             this.tbPnum.Name = "tbPnum";
             this.tbPnum.Size = new System.Drawing.Size(125, 27);
             this.tbPnum.TabIndex = 0;
             // 
             // tbQnum
             // 
-            this.tbQnum.Location = new System.Drawing.Point(85, 135);
+            this.tbQnum.Location = new System.Drawing.Point(85, 114);
             this.tbQnum.Name = "tbQnum";
             this.tbQnum.Size = new System.Drawing.Size(125, 27);
             this.tbQnum.TabIndex = 1;
             // 
             // tbDnum
             // 
-            this.tbDnum.Location = new System.Drawing.Point(85, 188);
+            this.tbDnum.Location = new System.Drawing.Point(85, 167);
             this.tbDnum.Name = "tbDnum";
             this.tbDnum.Size = new System.Drawing.Size(125, 27);
             this.tbDnum.TabIndex = 2;
@@ -65,7 +73,7 @@
             // lblP
             // 
             this.lblP.AutoSize = true;
-            this.lblP.Location = new System.Drawing.Point(85, 61);
+            this.lblP.Location = new System.Drawing.Point(85, 40);
             this.lblP.Name = "lblP";
             this.lblP.Size = new System.Drawing.Size(17, 20);
             this.lblP.TabIndex = 3;
@@ -74,7 +82,7 @@
             // lblQ
             // 
             this.lblQ.AutoSize = true;
-            this.lblQ.Location = new System.Drawing.Point(85, 114);
+            this.lblQ.Location = new System.Drawing.Point(85, 93);
             this.lblQ.Name = "lblQ";
             this.lblQ.Size = new System.Drawing.Size(20, 20);
             this.lblQ.TabIndex = 4;
@@ -83,7 +91,7 @@
             // lblD
             // 
             this.lblD.AutoSize = true;
-            this.lblD.Location = new System.Drawing.Point(85, 165);
+            this.lblD.Location = new System.Drawing.Point(85, 144);
             this.lblD.Name = "lblD";
             this.lblD.Size = new System.Drawing.Size(20, 20);
             this.lblD.TabIndex = 5;
@@ -91,7 +99,7 @@
             // 
             // btnKeyGen
             // 
-            this.btnKeyGen.Location = new System.Drawing.Point(85, 234);
+            this.btnKeyGen.Location = new System.Drawing.Point(85, 263);
             this.btnKeyGen.Name = "btnKeyGen";
             this.btnKeyGen.Size = new System.Drawing.Size(125, 29);
             this.btnKeyGen.TabIndex = 6;
@@ -101,14 +109,16 @@
             // 
             // tbRnum
             // 
-            this.tbRnum.Location = new System.Drawing.Point(250, 84);
+            this.tbRnum.Enabled = false;
+            this.tbRnum.Location = new System.Drawing.Point(250, 63);
             this.tbRnum.Name = "tbRnum";
             this.tbRnum.Size = new System.Drawing.Size(125, 27);
             this.tbRnum.TabIndex = 7;
             // 
             // tbEnum
             // 
-            this.tbEnum.Location = new System.Drawing.Point(250, 135);
+            this.tbEnum.Enabled = false;
+            this.tbEnum.Location = new System.Drawing.Point(250, 114);
             this.tbEnum.Name = "tbEnum";
             this.tbEnum.Size = new System.Drawing.Size(125, 27);
             this.tbEnum.TabIndex = 8;
@@ -116,7 +126,7 @@
             // lblR
             // 
             this.lblR.AutoSize = true;
-            this.lblR.Location = new System.Drawing.Point(250, 61);
+            this.lblR.Location = new System.Drawing.Point(250, 40);
             this.lblR.Name = "lblR";
             this.lblR.Size = new System.Drawing.Size(18, 20);
             this.lblR.TabIndex = 9;
@@ -125,17 +135,95 @@
             // lblE
             // 
             this.lblE.AutoSize = true;
-            this.lblE.Location = new System.Drawing.Point(250, 112);
+            this.lblE.Location = new System.Drawing.Point(250, 91);
             this.lblE.Name = "lblE";
             this.lblE.Size = new System.Drawing.Size(17, 20);
             this.lblE.TabIndex = 10;
             this.lblE.Text = "E";
             // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(250, 263);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(125, 29);
+            this.btnFile.TabIndex = 11;
+            this.btnFile.Text = "Select file";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(412, 63);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(192, 78);
+            this.rtbText.TabIndex = 12;
+            this.rtbText.Text = "";
+            this.rtbText.TextChanged += new System.EventHandler(this.rtbText_TextChanged);
+            // 
+            // tbHash
+            // 
+            this.tbHash.Location = new System.Drawing.Point(412, 167);
+            this.tbHash.Name = "tbHash";
+            this.tbHash.Size = new System.Drawing.Size(192, 27);
+            this.tbHash.TabIndex = 13;
+            // 
+            // tbSignature
+            // 
+            this.tbSignature.Location = new System.Drawing.Point(412, 217);
+            this.tbSignature.Name = "tbSignature";
+            this.tbSignature.Size = new System.Drawing.Size(192, 27);
+            this.tbSignature.TabIndex = 14;
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(412, 40);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(36, 20);
+            this.lblText.TabIndex = 15;
+            this.lblText.Text = "Text";
+            // 
+            // lblHash
+            // 
+            this.lblHash.AutoSize = true;
+            this.lblHash.Location = new System.Drawing.Point(412, 144);
+            this.lblHash.Name = "lblHash";
+            this.lblHash.Size = new System.Drawing.Size(42, 20);
+            this.lblHash.TabIndex = 16;
+            this.lblHash.Text = "Hash";
+            // 
+            // lblSignature
+            // 
+            this.lblSignature.AutoSize = true;
+            this.lblSignature.Location = new System.Drawing.Point(412, 197);
+            this.lblSignature.Name = "lblSignature";
+            this.lblSignature.Size = new System.Drawing.Size(72, 20);
+            this.lblSignature.TabIndex = 17;
+            this.lblSignature.Text = "Signature";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(412, 263);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(125, 29);
+            this.btnCheck.TabIndex = 18;
+            this.btnCheck.Text = "Check signature";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
             // DSForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 347);
+            this.ClientSize = new System.Drawing.Size(688, 347);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblSignature);
+            this.Controls.Add(this.lblHash);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.tbSignature);
+            this.Controls.Add(this.tbHash);
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.btnFile);
             this.Controls.Add(this.lblE);
             this.Controls.Add(this.lblR);
             this.Controls.Add(this.tbEnum);
@@ -168,5 +256,13 @@
         private TextBox tbEnum;
         private Label lblR;
         private Label lblE;
+        private Button btnFile;
+        private RichTextBox rtbText;
+        private TextBox tbHash;
+        private TextBox tbSignature;
+        private Label lblText;
+        private Label lblHash;
+        private Label lblSignature;
+        private Button btnCheck;
     }
 }
--- Lab4/RsaDS/DSForm.cs
+++ Lab4/RsaDS/DSForm.cs
@@ -21,12 +21,44 @@ namespace RsaDS
             InitializeComponent();
         }
 
-        private void DSForm_Load(object sender, EventArgs e)
+        private void DSForm_Load(object sender, EventArgs ev)
         {
 
         }
 
-        private void btnKeyGen_Click(object sender, EventArgs e)
+        private static BigInteger GetPublicKey(BigInteger a, BigInteger b)
+        {
+            BigInteger d0 = a;
+            BigInteger d1 = b;
+            BigInteger x0 = 1;
+            BigInteger x1 = 0;
+            BigInteger y0 = 0;
+            BigInteger y1 = 1;
+            BigInteger q;
+            BigInteger d2;
+            BigInteger x2;
+            BigInteger y2;
+
+            while (d1 > 1)
+            {
+                q = d0 / d1;
+                d2 = d0 % d1;
+                x2 = x0 - q * x1;
+                y2 = y0 - q * y1;
+                d0 = d1;
+                d1 = d2;
+                x0 = x1;
+                x1 = x2;
+                y0 = y1;
+                y1 = y2;
+            }
+            if (y1 > 0)
+                return y1;
+            else
+                return y1 + a;
+        }
+
+        private void btnKeyGen_Click(object sender, EventArgs ev)
         {
             try
             {
@@ -44,8 +76,8 @@ namespace RsaDS
                             tbRnum.Text = r.ToString();
                             e = GetPublicKey(PhiR, d);
                             tbEnum.Text = e.ToString();
-                            SelectFile.Enabled = true;
-                            button1.Enabled = true;
+                            btnFile.Enabled = true;
+                            btnFile.Enabled = true;
                         }
                         else
                             MessageBox.Show($"d должно быть взаимнопростым с (p - 1)(q - 1) = {PhiR}");
@@ -60,6 +92,108 @@ namespace RsaDS
             catch
             {
                 MessageBox.Show("p, q и d должны быть числами");
+            }
+        }
+
+        private static void SaveFile(String file, byte[] bytes, BigInteger sign)
+        {
+            FileStreamOptions options = new FileStreamOptions();
+            options.Access = FileAccess.Write;
+            options.Mode = FileMode.Create;
+            char[] chars = new char[bytes.Length];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                chars[i] = (char)bytes[i];
+            }
+            using (StreamWriter sw = new StreamWriter(file, options))
+            {
+                sw.WriteLine(sign.ToString());
+                sw.Write(chars);
+                sw.Close();
+            }
+        }
+
+        private void btnFile_Click(object sender, EventArgs ev)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            if (openFileDialog.FileName != "")
+            {
+                byte[] defaultText = ReadBytes(openFileDialog.FileName);
+                PrintDefaultText(defaultText);
+                BigInteger Hash = Helper.GetHash(defaultText, r);
+                tbHash.Text = Hash.ToString();
+                BigInteger S = BigInteger.ModPow(Hash, d, r);
+                tbSignature.Text = S.ToString();
+                SaveFile(openFileDialog.FileName, defaultText, S);
+            }
+        }
+
+        private static byte[] ReadBytes(String file)
+        {
+            using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+            {
+                byte[] bytes = new byte[fs.Length];
+                fs.Read(bytes, 0, bytes.Length);
+                fs.Close();
+                return bytes;
+            }
+        }
+
+        private void PrintDefaultText(byte[] bytes)
+        {
+            rtbText.Text = "";
+            foreach (byte b in bytes)
+            {
+                rtbText.Text += b.ToString() + " ";
+            }
+        }
+
+        private void rtbText_TextChanged(object sender, EventArgs ev)
+        {
+            /*tbEnum.Text = "";
+            tbRnum.Text = "";
+            rtbText.Text = "";
+            tbHash.Text = "";
+            tbSignature.Text = "";
+            btnFile.Enabled = false;
+            btnCheck.Enabled = false;*/
+        }
+
+        private void btnCheck_Click(object sender, EventArgs ev)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            if (openFileDialog.FileName != "")
+            {
+                using (StreamReader sr = new StreamReader(openFileDialog.FileName))
+                {
+                    try
+                    {
+                        BigInteger signInFile = BigInteger.Parse(sr.ReadLine());
+                        BigInteger hashInFile = BigInteger.ModPow(signInFile, e, r);
+                        char[] textInFileChars = sr.ReadToEnd().ToCharArray();
+                        byte[] textInFile = new byte[textInFileChars.Length];
+                        for (int i = 0; i < textInFileChars.Length; i++)
+                        {
+                            textInFile[i] = (byte)textInFileChars[i];
+                        }
+                        BigInteger Hash = Helper.GetHash(textInFile, r);
+                        if (Hash == hashInFile)
+                        {
+                            MessageBox.Show($"Подпись верна!\n Хэш файла = {Hash}\n Хэш из файла = {hashInFile}");
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Подпись не верна!\n Хэш файла = {Hash}\n Хэш из файла = {hashInFile}");
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show($"Ошибка при проверке файла");
+                    }
+
+                }
             }
         }
     }
